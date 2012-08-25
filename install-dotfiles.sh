65a2be51e7ac8a7bd771544d86f98b62e4c1fd2f
Mike McQuaid <mike@mikemcquaid.com>
2012-08-24 21:38:50 -0700

    Add and install Sublime Text 2 settings.

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,13 +5,37 @@ DOTFILESDIRREL=$(dirname $0)
 cd $DOTFILESDIRREL
 DOTFILESDIR=$(pwd -P)
 SCRIPTNAME=$(basename $0)
+
+[ $(uname -s) = "Darwin" ] && export OSX=1 && export UNIX=1
+[ $(uname -s) = "Linux" ] && export LINUX=1 && export UNIX=1
+uname -s | grep -q "_NT-" && export WINDOWS=1
+
 for DOTFILE in *; do
-	echo $DOTFILE | grep -q '\.' && continue
 	HOMEFILE="$HOME/.$DOTFILE"
 	[ -d $DOTFILE ] && DOTFILE="$DOTFILE/"
 	DIRFILE="$DOTFILESDIR/$DOTFILE"
-	# Matches Cygwin or MINGW
-	if ! uname -s | grep -q "_NT-"
+
+	if echo $DOTFILE | grep -q 'Preferences.sublime-settings'
+	then
+		if [ $OSX ]
+		then
+			SUBLIME="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
+			ln -sfv "$DIRFILE" "$SUBLIME/$DOTFILE"
+		elif [ $LINUX ]
+		then
+			SUBLIME="$HOME/.config/sublime-text-2/Packages/User"
+			ln -sfv "$DIRFILE" "$SUBLIME/$DOTFILE"
+		elif [ $WINDOWS ]
+		then
+			SUBLIME="$APPDATA/Sublime Text 2/Packages/User"
+			cp -rv  "$DIRFILE" "$SUBLIME/$DOTFILE"
+		fi
+		continue
+	fi
+
+	echo $DOTFILE | grep -q '\.' && continue
+
+	if [ $UNIX ]
 	then
 		if [ -L "$HOMEFILE" ] && ! [ -d $DOTFILE ]
 		then
