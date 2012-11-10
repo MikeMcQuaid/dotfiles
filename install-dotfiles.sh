a208848a0ee283a0cf4867199d363473784d4ba6
Mike McQuaid <mike@mikemcquaid.com>
2012-11-10 07:49:34 +0000

    Add Sublime Text 2 user configuration files.

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -10,30 +10,25 @@ SCRIPTNAME=$(basename $0)
 [ $(uname -s) = "Linux" ] && export LINUX=1 && export UNIX=1
 uname -s | grep -q "_NT-" && export WINDOWS=1
 
+if [ $OSX ]
+then
+	SUBLIME="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
+elif [ $LINUX ]
+then
+	SUBLIME="$HOME/.config/sublime-text-2/Packages/User"
+elif [ $WINDOWS ]
+then
+	SUBLIME="$APPDATA/Sublime Text 2/Packages/User"
+fi
+
 for DOTFILE in *; do
 	HOMEFILE="$HOME/.$DOTFILE"
 	[ -d $DOTFILE ] && DOTFILE="$DOTFILE/"
 	DIRFILE="$DOTFILESDIR/$DOTFILE"
 
-	if echo $DOTFILE | grep -q 'Preferences.sublime-settings'
-	then
-		if [ $OSX ]
-		then
-			SUBLIME="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
-			ln -sfv "$DIRFILE" "$SUBLIME/$DOTFILE"
-		elif [ $LINUX ]
-		then
-			SUBLIME="$HOME/.config/sublime-text-2/Packages/User"
-			ln -sfv "$DIRFILE" "$SUBLIME/$DOTFILE"
-		elif [ $WINDOWS ]
-		then
-			SUBLIME="$APPDATA/Sublime Text 2/Packages/User"
-			cp -rv  "$DIRFILE" "$SUBLIME/$DOTFILE"
-		fi
-		continue
-	fi
-
 	echo $DOTFILE | grep -q '\.' && continue
+
+	echo $DOTFILE | grep -q 'sublime' && HOMEFILE="$SUBLIME"
 
 	if [ $UNIX ]
 	then
@@ -45,6 +40,6 @@ for DOTFILE in *; do
 			ln -sv "$DIRFILE" "$HOMEFILE"
 		fi
 	else
-		cp -rv "$DIRFILE" "$HOMEFILE"
+		echo cp -rv "$DIRFILE" "$HOMEFILE"
 	fi
 done
