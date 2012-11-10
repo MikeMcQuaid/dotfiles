7048d161a0992411447030d709823f26d6dfae0d
Mike McQuaid <mike@mikemcquaid.com>
2012-11-10 08:13:10 +0000

    Improve Curl non-Git support.

--- update-from-github-tarball.sh
+++ update-from-github-tarball.sh
@@ -14,9 +14,9 @@ then
 fi
 
 OUTFILE="dotfiles.tar.gz"
-which wget >/dev/null && DOWNLOAD="wget --no-check-certificate --output-document=$OUTFILE"
-[ -z "$DOWNLOAD" ] && which curl >/dev/null && DOWNLOAD="curl --location --output $OUTFILE"
-[ -z "$DOWNLOAD" ] && echo "Could not find wget or curl" && exit 1
+which curl &>/dev/null && DOWNLOAD="curl --progress-bar --location --output $OUTFILE"
+[ -z "$DOWNLOAD" ] && which wget &>/dev/null && DOWNLOAD="wget --output-document=$OUTFILE"
+[ -z "$DOWNLOAD" ] && echo "Could not find curl or wget" && exit 1
 
 $DOWNLOAD https://github.com/mikemcquaid/dotfiles/tarball/master
 tar --strip-components=1 -z -x -v -f dotfiles.tar.gz
