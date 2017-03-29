c0e998af6cf393ec1d453da22e13ab3cff4d73c8
Mike McQuaid <mike@mikemcquaid.com>
2017-03-29 19:50:28 +0100

    Correct handle interactive and login shells.
    Rather than assuming `bashrc`, `bash_profile` or `zprofile` are loaded
    or not based on platform correctly check whether a shell is a login
    and/or interactive shell and load files accordingly.

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,6 +1,12 @@
 # load shared shell configuration
 source ~/.shprofile
 
+# run bashrc if this is a login, interactive shell
+if [ "$0" = "-bash" ] && echo "$-" | grep -q "i"
+then
+  source ~/.bashrc
+fi
+
 # Set HOST for ZSH compatibility
 export HOST=$HOSTNAME
 
