cb8810e7a4b41b6b148b0bf2ded207a3677e8d3e
Cayce House <cayce@houseco.cc>
2019-02-19 21:59:05 -0500

    Removed non essential config

--- zshrc.sh
+++ zshrc.sh
@@ -1,36 +1,50 @@
-# check if this is a login shell
-[ "$0" = "-zsh" ] && export LOGIN_ZSH=1
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
 
-# run zprofile if this is not a login shell
-[ -n "$LOGIN_ZSH" ] && source ~/.zprofile
+# Clear packages
+zplug clear
 
-# load shared shell configuration
-source ~/.shrc
+# Package list
+zplug desyncr/auto-ls
+zplug dracula/zsh, as:theme
+zplug peterhurford/up.zsh
+zplug zsh-users/zsh-autosuggestions
+zplug zsh-users/zsh-completions
+zplug zsh-users/zsh-syntax-highlighting
 
-# History file
-export HISTFILE=~/.zsh_history
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
 
-# Don't show duplicate history entires
-setopt hist_find_no_dups
+# Then, source plugins and add commands to $PATH
+zplug load
 
-# Remove unnecessary blanks from history
-setopt hist_reduce_blanks
+# Configure history
+HISTFILE="${HOME}/.zsh_history"
+HISTSIZE=10000
+SAVEHIST=10000
+setopt INC_APPEND_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt EXTENDED_HISTORY
 
-# Share history between instances
-setopt share_history
+# Make Homebrew safer
+export HOMEBREW_CASK_OPTS=--require-sha
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_INSECURE_REDIRECT=1
 
-# Don't hang up background jobs
-setopt no_hup
+# Set FZF command
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
 
-# use emacs bindings even with vim as EDITOR
-bindkey -e
+# Aliases
+alias ls='exa'
+alias l='exa -l --git'
 
-# fix backspace on Debian
-[ -n "$LINUX" ] && bindkey "^?" backward-delete-char
+# Alias hub over git
+eval "$(hub alias -s)"
 
-# fix delete key on macOS
-[ -n "$MACOS" ] && bindkey '\e[3~' delete-char
-
-# alternate mappings for Ctrl-U/V to search the history
-bindkey "^u" history-beginning-search-backward
-bindkey "^v" history-beginning-search-forward
+# Initialize rbenv
+eval "$(rbenv init -)"
