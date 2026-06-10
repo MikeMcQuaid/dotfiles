f6b0e8cd9190b2e5b099b30db7ed70940fbf5f9f
Mike McQuaid <mike@mikemcquaid.com>
2026-06-10 20:56:38 +0100

    shrc: remove deprecated Homebrew variables.

--- shrc.sh
+++ shrc.sh
@@ -93,12 +93,10 @@ alias desk_sit="upsy-desky down"
 # Command-specific stuff
 if quiet_which brew; then
   export HOMEBREW_DEVELOPER=1
-  export HOMEBREW_BUNDLE_DESCRIBE=1
   export HOMEBREW_BUNDLE_CHECK=1
   export HOMEBREW_BUNDLE_NO_SECRETS=1
   export HOMEBREW_NO_ENV_HINTS=1
   export HOMEBREW_CLEANUP_MAX_AGE_DAYS=30
-  export HOMEBREW_USE_INTERNAL_API=1
   export HOMEBREW_UPGRADE_GREEDY_CASKS="claude-code codex zed"
   export HOMEBREW_BUNDLE_JOBS=auto
   export HOMEBREW_REQUIRE_TAP_TRUST=1
